d9c1b2ec57e26010b836a3557e1480272ffeab39
kedap <danielxdap@gmail.com>
2021-05-23 20:44:52 -0500

    Primera version?

--- pyarch.py
+++ pyarch.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-# Script de instalacion en python para arch
+# Script de instalacion en python para arch y solo sistemas efi
 # https://github.com/kedap/pyarch
 
 import os, time
@@ -77,7 +77,7 @@ def particion_de_discos():
 		particion_de_discos()
 	system("cfdisk " + disco)
 	subtitulo("Particion de discos", "Seleccionando particiones")
-	particion = ["Hola", "Hola", "Hola", "Hola"]
+	particion = ["boot", "swap", "root", "home"]
 	particion[0] = input("Particion boot [/dev/xxx1] ")
 	particion_swap = pregunta("Vas a utilizar una particion swap?")
 	if particion_swap == True:
@@ -92,7 +92,113 @@ def particion_de_discos():
 	else:
 		info("No se va a utilizar una particion home...")
 		particion[3] = False
-	return particion
 
+	subtitulo("Particion de discos", "Verificacion de particiones")
+	print("Es correcta la tabla de particiones?")
+	print(particion[0] + "\tBooteable\tMas de 260Mib (particion boot)\tDe tipo linux")
+	if particion[1] == False:
+		print("No Seleccionada (no se va a usar)\tNo Booteable\tMas de 512Mib (particion swap)\tDe tipo swap linux")
+	else:
+		print(particion[1] + "\tNo Booteable\tMas de 512Mib (particion swap)\tDe tipo swap linux")
+	if particion[3] == False:
+		print(particion[2] + "\tNo Booteable\tDepende de su gusto (particion root y home)\tDe tipo linux")
+	else:
+		print(particion[2] + "\tNo Booteable\tDepende de su gusto (particion root)\tDe tipo linux")
+		print(particion[3] + "\tNo Booteable\tDepende de su gusto (particion home)\tDe tipo linux")
+	continuar = pregunta("Estas seguro de continuar con este esquema de particiones?")
+	if continuar == True:
+		return particion
+	else:
+		error("Ok... Abortando :(")
+		exit(1)
+
+def formateo_partciones(particiones_arr):
+	banner_titulo("Formateo de partciones")
+	info("Iniciando el formateo de partciones...")
+	info("Formateando en ext4...")
+	dual_boot = pregunta("Se va a instalar junto a otro OS? (dual boot)")
+	if dual_boot == True:
+		info("No se va a formatear la partcion boot")
+	else:
+		info("Fomartenado ext4 a la partcion boot [" + particiones_arr[0] + "]...")
+		system("mkfs.ext4 " + particiones_arr[0])
+	if particiones_arr[1] == False:
+		info("No se creara un particion swap, puesto a que no se creo.")
+	else:
+		info("Creando partcion swaaap en la partcion " + particiones_arr[1])
+		system("mkswap " + particiones_arr[1])
+		info("Activando la particion swap")
+		system("swapon " + particiones_arr[1])
+	if particiones_arr[3] == False:
+		info("Se utilizara una particion para root y home...")
+		info("Formatiando la partcion con ext4 " + particiones_arr[2])
+		system("mkfs.ext4 " + particiones_arr[2])
+	else:
+		info("Utilizando la particion " + particiones_arr[2] + " Para root")
+		info("Utilizando la particion " + particiones_arr[3] + " Para home")
+		info("Formatiando la partcion " + particiones_arr[2])
+		system("mkfs.ext4 " + particiones_arr[2])
+		info("Formatiando la particion " + particiones_arr[3])
+		system("mkfs.ext4 " + particiones_arr[3])
+	info("El formateo de partciones se ejecuto de manera correcta!")
+
+def montado_partciones(particiones_arr):
+	banner_titulo("Montando particiones")
+	info("Montando partciones...")
+	info("Montando la partcion efi")
+	system("mkdir -p /mnt/efi")
+	system("mount " + particiones_arr[0] + " /mnt/efi")
+	if particiones_arr[3] == False:
+		info("Montando la partcion root...")
+		system("mount " + particiones_arr[2] + " /mnt")
+	else:
+		info("Montando la partcion home...")
+		system("mkdir -p /mnt/home")
+		system("mount " + particiones_arr[3] + " /mnt/home")
+		info("Montando la partcion root...")
+		system("mount " + particiones_arr[2] + " /mnt")
+	info("La montancion de partciiones a sido correcta")
+
+def descarga():
+	banner_titulo("Descarga de archlinux")
+	network = pregunta("Quieres instalar networkmanager?")
+	if network == True:
+		system("pacstrap /mnt base linux linux-firmware vim networkmanager grub")
+	else:
+		system("pacstrap /mnt base linux linux-firmware vim grub")
+
+def idioma():
+	banner_titulo("Generando locales")
+	print("Elimina tu idioma y pais")
+	input("Enter cuando estes listo")
+	system("vim /etc/locale.gen")
+	locale = input("Ingresa tu nombre e idioma [es_ES.UTF-8]")
+	system("echo 'LANG=" + locale + "' > /etc/locale.conf")
+	info("Generando locales...")
+	system("locale-gen")
+
+def host():
+	banner_titulo("Informacion host")
+	hostname = input("Ingresa el nombre de tu equipo")
+	system("echo " + hostname + " > /etc/hots")
+	info("Modificando el archivo /etc/hosts")
+	system('echo -e "127.0.0.1\tlocalhost" >> /etc/hosts')
+	system('echo -e "::1\tlocalhost" >> /etc/hosts')
+	system('echo -e "127.0.0.1\t' + hostname + '" >> /etc/hosts')
 
 tabla_de_particion = particion_de_discos()
+formateo_partciones(tabla_de_particion)
+montado_partciones(tabla_de_particion)
+descarga()
+system("genfstab -U /mnt >> /mnt/etc/fstab")
+info("Cambiando al nuevo entorno root...")
+system("arch-chroot /mnt")
+idioma()
+hosts()
+system("mkinitcpio -P")
+info("Creando contraseña root")
+system("passwd")
+info("instalando grub...")
+system("grub-install --target=x86_64 --efi-directory=/efi --bootloader-id=GRUB")
+system("grub-mkconfig -o /boot/grub/grub.cfg")
+info("Instalacion completa!")
